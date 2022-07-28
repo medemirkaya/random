9640104e1280cd36a3f3e41623ed488ef79a42d4
medemirkaya <medemirkaya@outlook.com>
2022-07-28 15:55:48 +0300

    first commit

---
+++ random_generator.rb
@@ -0,0 +1,18 @@
+puts "Random Password Generator is working"
+
+def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z') + Array(0..9)  + Array('!'..'?')
+    Array.new(number) { charset.sample }.join
+  end
+  puts"How much digits?"
+  basamak = gets.to_i
+  puts "How much times?"
+  times = gets.to_i
+  timesEnd = 1
+
+  while times >= timesEnd
+    puts generate_code(basamak)
+    timesEnd +=1
+  end
+  
+  
